47919ae2c8a7bd855e96e1ec01e9fbea2ee40cbe
Bruna del Mar <bruxsea.9.3.22@gmail.com>
2024-12-16 22:01:02 -0300

    funcion del boton continuar lista

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ options.forEach(option=> {
     option.addEventListener('click', selectOption);
 });
 
+
+// Funcion para el boton continue, para cambiar de vista
+const steps = [
+    document.getElementById('container-step-one'),
+    document.getElementById('container-step-two'),
+    document.getElementById('container-step-three'),
+];
+
+const continueButtons = document.querySelectorAll('.btn-continue');
+
+let currentStep = 0; // step actual. Esta ne 0 porq comienza en el indice del array cero
+
+function changeStep() {
+    if(currentStep < steps.length -1) {
+        steps[currentStep].classList.add('hidden');
+
+        currentStep++;
+        steps[currentStep].classList.remove('hidden');
+    }
+}
+
+continueButtons.forEach(button => {
+    button.addEventListener('click', changeStep);
+});
+
